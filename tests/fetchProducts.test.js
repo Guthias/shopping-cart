73664fcffb7bce11901b3fdaa41c41461308b793
guthias <gmgustavom@gmail.com>
2022-01-14 16:51:23 -0300

    Escrevendo testes para o fetchProducts

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -6,5 +6,10 @@ describe('1 - Teste a função fecthProducts', () => {
   it('Verifica-se fetchProducts é uma função', () => {
     expect(typeof fetchProducts).toBe('function');
   });
+
+  it(`Verifica-se a função fetchProducts é chamada com o argumento 'Computador'`, async () => {
+    await fetchProducts('computador')
+    expect(fetch).toHaveBeenCalled()
+  })
   fail('Teste incompleto');
 });
