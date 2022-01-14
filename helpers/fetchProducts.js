a4fc7faee7839936d48100f604e015bf5540bc96
guthias <gmgustavom@gmail.com>
2022-01-14 17:43:37 -0300

    Corrigindo fetchProducts para retornar o objeto esperado

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -5,7 +5,7 @@ const fetchProducts = async (query) => {
     const response = await fetch(url);
     const data = await response.json();
     // console.log(data.results);
-    return data.results;
+    return data;
   } catch (error) {
     throw new Error('');
   }
