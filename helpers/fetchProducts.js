6a7d97cebb6d5dc9a6300e88ac7c754202d1bb3f
guthias <gmgustavom@gmail.com>
2022-01-14 22:01:24 -0300

    Refatorando function fetchProducts

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,21 +1,16 @@
 const fetchProducts = async (query) => {
-  if (!query) {
-    throw new Error('You must provide an url');
-  }
-
   const url = `https://api.mercadolibre.com/sites/MLB/search?q=${query}`;
 
   try {
+    if (!query) throw new Error('You must provide an url');
+    
     const response = await fetch(url);
     const data = await response.json();
-    // console.log(data.results);
     return data;
   } catch (error) {
     throw new Error(error);
   }
 };
-
-// fetchProducts();
 
 if (typeof module !== 'undefined') {
   module.exports = {
