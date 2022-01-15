464e5fbecfb58ae5d525e76294f1aa46fbd28803
guthias <gmgustavom@gmail.com>
2022-01-15 20:39:47 -0300

    Testando se fetchItem é chamada ao passar um argumento

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -6,4 +6,9 @@ describe('2 - Teste a função fecthItem', () => {
   it('Verifica-se fetchItem é uma função', () => {
     expect(typeof fetchItem).toBe('function');
   });
+
+  it(`Verifica-se a função fetchProducts é chamada com o argumento 'MLB1615760527'`, async () => {
+    await fetchItem('MLB1615760527')
+    expect(fetch).toHaveBeenCalled()
+  });
 });
