51a7acc05627646d12e94bbe1db9de9228a86f6d
guthias <gmgustavom@gmail.com>
2022-01-16 18:55:02 -0300

    Testando se a API do Mercado Livre esta sendo consultada na function fetchItem

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -7,8 +7,13 @@ describe('2 - Teste a função fecthItem', () => {
     expect(typeof fetchItem).toBe('function');
   });
 
-  it(`Verifica-se a função fetchProducts é chamada com o argumento 'MLB1615760527'`, async () => {
-    await fetchItem('MLB1615760527')
+  it(`Verifica-se a função fetchItem é chamada com o argumento 'MLB1615760527'`, async () => {
+    await fetchItem('MLB1615760527');
     expect(fetch).toHaveBeenCalled()
   });
+
+  it('Verifica-se a APi do Mercado Livre está sendo consultada na function fetchItem', async () => {
+    await fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/items/MLB1615760527');
+  })
 });
