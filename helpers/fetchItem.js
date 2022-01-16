f9b0a604c8dcdceee904f11b6097034f3741499d
guthias <gmgustavom@gmail.com>
2022-01-16 20:09:35 -0300

    Retornando erro em FetchItem ao não receber um parametro

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -2,6 +2,8 @@ const fetchItem = async (itemID) => {
   const url = `https://api.mercadolibre.com/items/${itemID}`;
 
   try {
+    if (!itemID) throw new Error('You must provide an URL');
+
     const response = await fetch(url);
     const data = await response.json();
     return data;
