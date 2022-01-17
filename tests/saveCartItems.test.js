e7f564b3a14ade05f65aee92a5411c2a61ffccdd
guthias <gmgustavom@gmail.com>
2022-01-17 00:21:50 -0300

    Escrevendo teste para verificar-se saveCartItems usa o metodo localStorage.setItem

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -4,6 +4,8 @@ const saveCartItems = require('../helpers/saveCartItems');
 localStorageSimulator('setItem');
 
 describe('4 - Teste a função saveCartItems', () => {
-  // implemente seus testes aqui
+  it('Verifica-se getSavedCartItems com argumentos chama o metodo localStorage.setItem', () => {
+    expect(saveCartItems('<ol><li>Item</li></ol>')).toHaveBeenCalled(localStorage.setItem());
+  });
   fail('Teste vazio');
 });
