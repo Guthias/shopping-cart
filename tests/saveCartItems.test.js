c7542d9e16440ca0921f1ac7f1086cdf85c8d2f0
guthias <gmgustavom@gmail.com>
2022-01-17 01:38:45 -0300

    Escrevendo teste para verificar se saveCartItems converte para JSON ao receber um array

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -1,5 +1,6 @@
 const localStorageSimulator = require('../mocks/localStorageSimulator');
 const saveCartItems = require('../helpers/saveCartItems');
+const { stringify } = require('mocha/lib/utils');
 
 localStorageSimulator('setItem');
 
@@ -13,5 +14,21 @@ describe('4 - Teste a função saveCartItems', () => {
     const content = '<ol><li>Item</li></ol>';
     saveCartItems(content);
     expect(localStorage.setItem).toHaveBeenCalledWith('cartItems', content);
+  });
+
+  it('Verifica-se saveCartItems ao receber um array converte o conteudo para JSON', () => {
+    const content = [
+      { 
+        sku: "MLB1937079157", 
+        name: "Pc Computador Cpu Core I5 650 + Ssd 240gb, 8gb Memória Ram",
+        salePrice: 1439 
+      }, {
+        sku: "MLB1607748387",
+        name: "Pc Computador Cpu Intel Core I5 + Ssd 240gb, 8gb Memória Ram",
+        salePrice: 1720.19 
+      }
+    ];
+    saveCartItems(content);
+    expect(localStorage.setItem).toHaveBeenCalledWith('cartItems', JSON.stringify(content));
   });
 });
